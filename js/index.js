f1c8c5d8044968542d9fb0f006c4f8ea1d66b0ab
Tyler <tkicks32@gmail.com>
2014-10-19 23:17:16 -0400

    updated text in progress bar and map

--- js/index.js
+++ js/index.js
@@ -109,22 +109,25 @@ $(function() {
 
 
 // real time ETA progress bar
+// doesn't use jquery b/c adapted from my codepen I did in regular js
 $(function() {
 
 	var today = new Date(),					// today's date
 		dayHeLeft = new Date(2014, 4, 1),	// date he left
 		totalDays = 457,					// total number of days he's gone
 		daysBeenGone = today - dayHeLeft,	// # days gone so far
-		percent;							// percent of time completed
+		percent,							// percent of time completed
+		daysLeft;							// # days left of sabbatical
 
 	// convert number of days he's been gone to days from milliseconds
 	// check in console to make sure right
     daysBeenGone = ((((daysBeenGone/1000)/60)/60)/24);
     console.log(daysBeenGone);
 
+    daysLeft = Math.floor(totalDays - daysBeenGone);
 
     // if he's already back fill up entire bar and say he's been gone
-    if ((totalDays - daysBeenGone) <= 0) {
+    if ((daysLeft) <= 0) {
 		document.getElementById('progressbar').style.width=100 + "%";
         document.getElementById('progressbar').innerHTML="Mark's returned";
     }
@@ -137,7 +140,7 @@ $(function() {
         }
 
         console.log(percent);
-        document.getElementById('progressbar').style.width=percent + "%";
-        document.getElementById('progressbar').innerHTML=percent + "% of sabbatical completed";
+        document.getElementById('progressbar').style.width = percent + "%";
+        document.getElementById('progressbar').innerHTML = percent + "% of sabbatical completed.  " + daysLeft + " days left";
     }
 })
